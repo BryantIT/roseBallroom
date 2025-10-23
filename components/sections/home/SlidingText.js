38a884ac295362e953c3ccb5324b99afe3b6e80f
Bryant Richards <dragonseyeimages@gmail.com>
2025-10-22 19:02:48 -0500

    update sliding text UI

--- components/sections/home/SlidingText.js
+++ components/sections/home/SlidingText.js
@@ -1,52 +1,50 @@
 export default function SlidingText() {
-  
-
   return (
     <>
       {/* Sliding Text Start */}
-      <section className="sliding-text-one">
-          <div className="sliding-text-one__wrap">
-          <ul className="sliding-text__list marquee_mode">
-              <li>
-              <h2 data-hover="Magic of Events" className="sliding-text__title">
-                  Magic of Events
-                  <img src="/assets/images/icon/star-icon.png" alt="Star Icon" />
+      <section className='sliding-text-one'>
+        <div className='sliding-text-one__wrap'>
+          <ul className='sliding-text__list marquee_mode'>
+            <li>
+              <h2 data-hover='Waltz' className='sliding-text__title'>
+                Waltz
+                <img src='/assets/images/icon/star-icon.png' alt='Star Icon' />
               </h2>
-              </li>
-              <li>
-              <h2 data-hover="Celebrate Life" className="sliding-text__title">
-                  Celebrate Life
-                  <img src="/assets/images/icon/star-icon.png" alt="Star Icon" />
+            </li>
+            <li>
+              <h2 data-hover='Rumba' className='sliding-text__title'>
+                Rumba
+                <img src='/assets/images/icon/star-icon.png' alt='Star Icon' />
               </h2>
-              </li>
-              <li>
-              <h2 data-hover="Magic of Events" className="sliding-text__title">
-                  Magic of Events
-                  <img src="/assets/images/icon/star-icon.png" alt="Star Icon" />
+            </li>
+            <li>
+              <h2 data-hover='Foxtrot' className='sliding-text__title'>
+                Foxtrot
+                <img src='/assets/images/icon/star-icon.png' alt='Star Icon' />
               </h2>
-              </li>
-              <li>
-              <h2 data-hover="Celebrate Life" className="sliding-text__title">
-                  Celebrate Life
-                  <img src="/assets/images/icon/star-icon.png" alt="Star Icon" />
+            </li>
+            <li>
+              <h2 data-hover='Swing' className='sliding-text__title'>
+                Swing
+                <img src='/assets/images/icon/star-icon.png' alt='Star Icon' />
               </h2>
-              </li>
-              <li>
-              <h2 data-hover="Magic of Events" className="sliding-text__title">
-                  Magic of Events
-                  <img src="/assets/images/icon/star-icon.png" alt="Star Icon" />
+            </li>
+            <li>
+              <h2 data-hover='Tango' className='sliding-text__title'>
+                Tango
+                <img src='/assets/images/icon/star-icon.png' alt='Star Icon' />
               </h2>
-              </li>
-              <li>
-              <h2 data-hover="Celebrate Life" className="sliding-text__title">
-                  Celebrate Life
-                  <img src="/assets/images/icon/star-icon.png" alt="Star Icon" />
+            </li>
+            <li>
+              <h2 data-hover='Merengue' className='sliding-text__title'>
+                Merengue
+                <img src='/assets/images/icon/star-icon.png' alt='Star Icon' />
               </h2>
-              </li>
+            </li>
           </ul>
-          </div>
+        </div>
       </section>
       {/* Sliding Text End */}
     </>
-  );
+  )
 }
